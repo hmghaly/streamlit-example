bbfad06a8158e40946b8b2b41e949b64e9e66c79
Hussein Ghaly <hmghaly@users.noreply.github.com>
2023-11-17 10:50:37 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,6 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-"""
-# مرحبا بكم في البرمجان
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 st.write("مرحبا بكم في البرمجان")
 st.button('Hit me')
 input0= st.text_input('Enter some text')
